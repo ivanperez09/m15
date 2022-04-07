eb45eadc33e6e9219c2fc835b327b4a600ee8ea9
ivanperez09 <72545746+ivanperez09@users.noreply.github.com>
2022-04-07 19:24:06 +0200

    d

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let video;
+/*let video;
 let poseNet;
 let poses = [];
 let nose
@@ -16,9 +16,7 @@ function setup() {
   leftear = loadImage('images/leftear.png');
   rightear = loadImage('images/rightear.png');
 
-
   poseNet = ml5.poseNet(video, modelReady);
-
 
   poseNet.on('pose', function(results) {
     poses = results;
@@ -52,13 +50,14 @@ function drawKeypoints()  {
       if (keypoint.score > 0.2) {
         fill(255, 0, 0);
         noStroke();
-        image(eyes, keypoint.leftEye.x, keypoint.leftEye.y, 30, 30);
-        image(eyes, keypoint.rightEye.x, keypoint.rightEye.y, 30, 30);
-        image(nose, keypoint.nose.x, keypoint.nose.y, 30, 30);
-        image(leftear, keypoint.leftEar.x, keypoint.leftEar.y, 30, 30);
-        image(rightear, keypoint.rightEar.x, keypoint.rightEar.y, 30, 30);
+
+        image(eyes, keypoint.leftEye.x+1, keypoint.leftEye.y+1, 30, 30);
+        image(eyes, keypoint.rightEye.x+1, keypoint.rightEye.y+1, 30, 30);
+        image(nose, keypoint.nose.x+1, keypoint.nose.y+1, 30, 30);
+        image(leftear, keypoint.leftEar.x+1, keypoint.leftEar.y+1, 30, 30);
+        image(rightear, keypoint.rightEar.x+1, keypoint.rightEar.y+1, 30, 30);
+        
       }
-      
     }
   }
 }
@@ -72,8 +71,77 @@ function drawSkeleton() {
     for (let j = 0; j < skeleton.length; j++) {
       let partA = skeleton[j][0];
       let partB = skeleton[j][1];
-      stroke(255, 0, 0);
+      stroke(255, 255, 0);
       line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
     }
   }
+}*/
+
+let capture;
+let posenet;
+let nose
+let eyes
+let leftear
+let rightea
+let singlePose,skeleton;
+let actor_img;
+let specs,smoke;
+
+function setup() {  // this function runs only once while running
+    createCanvas(800, 500);
+    //console.log("setup funct");
+    capture = createCapture(VIDEO);
+    capture.hide();
+
+    //load the PoseNet model
+    posenet = ml5.poseNet(capture, modelLOADED);
+    //detect pose
+    posenet.on('pose', recievedPoses);
+
+    eyes = loadImage('images/eyes.png');
+    nose = loadImage('images/nose.png');
+    leftear = loadImage('images/leftear.png');
+    rightear = loadImage('images/rightear.png');
 }
+
+function recievedPoses(poses) {
+    console.log(poses);
+
+    if(poses.length > 0) {
+        singlePose = poses[0].pose;
+        skeleton = poses[0].skeleton;
+    }
+}
+
+function modelLOADED() {
+    console.log("model has loaded");
+}
+
+
+function draw() { 
+    
+    image(capture, 0, 0);
+    fill(255, 0, 0);
+    
+    if(singlePose) {
+        for(let i=0; i<singlePose.keypoints.length; i++) {
+            ellipse(singlePose.keypoints[i].position.x, singlePose.keypoints[i].position.y, 20);
+        }
+
+        stroke(255, 255, 255);
+        strokeWeight(5);
+
+        for(let j=0; j<skeleton.length; j++) {
+            line(skeleton[j][0].position.x, skeleton[j][0].position.y, skeleton[j][1].position.x, skeleton[j][1].position.y);
+        }
+
+        image(eyes, keypoint.leftEye.x+1, keypoint.leftEye.y+1, 30, 30);
+        image(eyes, keypoint.rightEye.x+1, keypoint.rightEye.y+1, 30, 30);
+        image(nose, keypoint.nose.x+1, keypoint.nose.y+1, 30, 30);
+        image(leftear, keypoint.leftEar.x+1, keypoint.leftEar.y+1, 30, 30);
+        image(rightear, keypoint.rightEar.x+1, keypoint.rightEar.y+1, 30, 30);;
+    }
+    
+  }
+
+
